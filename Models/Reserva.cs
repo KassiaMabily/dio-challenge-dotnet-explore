37db558615424f4be41b18268e3cf225e19458bd
Kassia Fraga <kassia.fraga@lumaensino.com.br>
2023-10-14 22:44:41 -0300

    Challenge solved!

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,17 +15,9 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
-            {
-                Hospedes = hospedes;
-            }
-            else
-            {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
-            }
+            if(hospedes.Count > Suite.Capacidade) throw new Exception("Quantidade de hospedes maior que a capacidade da suíte!");
+
+            Hospedes = hospedes;
         }
 
         public void CadastrarSuite(Suite suite)
@@ -35,26 +27,24 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            // Retorna a quantidade de hóspedes (propriedade Hospedes)
+            return Hospedes.Count();
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            // Retorna o valor da diária
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
-            {
-                valor = 0;
-            }
+            // Usando logica
+            int vaiAplicarDesconto = Convert.ToInt32(DiasReservados >= 10);
+            decimal valorDesconto = vaiAplicarDesconto * (valor * 0.1M);
 
-            return valor;
+            // Usando operador condicional
+            // decimal valorDesconto = DiasReservados >= 10 ? (valor * 0.1M) : 0;
+
+            // Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            return valor - valorDesconto;
         }
     }
 }
